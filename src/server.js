330dbee4c33dec41e82430e49c3604e8e02cfc2e
Chris <cxg8590@rit.edu>
2017-03-24 06:06:26 -0400

    finishing up a few things and adding the circle file

--- src/server.js
+++ src/server.js
@@ -7,9 +7,9 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const index = fs.readFileSync('client/index.html');
 
 const onRequest = (request, response) => {
-	response.writeHead(200, {'Content-Type': 'text/html'});
-	response.write(index);
-	response.end();
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(index);
+  response.end();
 };
 
 const app = http.createServer(onRequest).listen(port);
@@ -19,59 +19,59 @@ console.log(`Listening on 127.0.0.1: ${port}`);
 const io = socketio(app);
 
 class Room {
-    constructor() {
-        this.players = 0;
-    }
+  constructor() {
+    this.players = 0;
+  }
 }
 
 const rooms = {};
 
-var room = "room1";
+let room = 'room1';
 
 const onJoined = (sock) => {
-	const socket = sock;
-	
-	socket.on('join', (data) => {
-        room = data;
-        console.log("room: " + room);
-        if(!rooms[room]) rooms[room] = new Room();
-        rooms[room].players++;
-        if(rooms[room].players == 1){
-            socket.join(data);
-            io.sockets.in(room).emit('areYouX', true);
-        }
-        if(rooms[room].players == 2){
-            socket.join(data);
-            io.sockets.in(room).emit('areYouX', false);
-        }
-	});
+  const socket = sock;
+
+  socket.on('join', (data) => {
+    room = data;
+    console.log(`room: ${room}`);
+    if (!rooms[room]) rooms[room] = new Room();
+    rooms[room].players += 1;
+    if (rooms[room].players === 1) {
+      socket.join(data);
+      io.sockets.in(room).emit('areYouX', true);
+    }
+    if (rooms[room].players === 2) {
+      socket.join(data);
+      io.sockets.in(room).emit('areYouX', false);
+    }
+  });
 };
 
 const onTurn = (sock) => {
-	const socket = sock;
-    
-    socket.on('boardPick', (data) => {
-        io.sockets.in(room).emit('board', data);
-    })
-	
-	socket.on('playerTurn', (data) => {
-		io.sockets.in(room).emit('Turn', data);
-	});
+  const socket = sock;
+
+  socket.on('boardPick', (data) => {
+    io.sockets.in(room).emit('board', data);
+  });
+
+  socket.on('playerTurn', (data) => {
+    io.sockets.in(room).emit('Turn', data);
+  });
 };
 
 const onDisconnect = (sock) => {
-	const socket = sock;
-	socket.on('disconnect', () => {
-        console.log("disconnect");
-		socket.leave(room);
-        rooms[room].players--;
-	});
+  const socket = sock;
+  socket.on('disconnect', () => {
+    console.log('disconnect');
+    socket.leave(room);
+    rooms[room].players -= 1;
+  });
 };
 
 io.sockets.on('connection', (socket) => {
-	console.log('started');
-	
-	onJoined(socket);
-	onTurn(socket);
-	onDisconnect(socket);
+  console.log('started');
+
+  onJoined(socket);
+  onTurn(socket);
+  onDisconnect(socket);
 });
