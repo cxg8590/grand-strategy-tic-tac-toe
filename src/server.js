88c35c2ea407c8a07fd7456fba67e087790882f7
Chris <cxg8590@rit.edu>
2017-03-24 05:38:24 -0400

    Finished project. Would have committed earlier but there were some significant bugs and I didnt really stop working

--- src/server.js
+++ src/server.js
@@ -18,28 +18,53 @@ console.log(`Listening on 127.0.0.1: ${port}`);
 
 const io = socketio(app);
 
+class Room {
+    constructor() {
+        this.players = 0;
+    }
+}
+
+const rooms = {};
+
 var room = "room1";
 
 const onJoined = (sock) => {
 	const socket = sock;
 	
 	socket.on('join', (data) => {
-		socket.join(data);
+        room = data;
+        console.log("room: " + room);
+        if(!rooms[room]) rooms[room] = new Room();
+        rooms[room].players++;
+        if(rooms[room].players == 1){
+            socket.join(data);
+            io.sockets.in(room).emit('areYouX', true);
+        }
+        if(rooms[room].players == 2){
+            socket.join(data);
+            io.sockets.in(room).emit('areYouX', false);
+        }
 	});
 };
 
-const onDrw = (sock) => {
+const onTurn = (sock) => {
 	const socket = sock;
+    
+    socket.on('boardPick', (data) => {
+        io.sockets.in(room).emit('board', data);
+    })
 	
-	socket.on('draw', (data) => {
-		io.sockets.in(room).emit('update', data);
+	socket.on('playerTurn', (data) => {
+		io.sockets.in(room).emit('Turn', data);
 	});
 };
 
 const onDisconnect = (sock) => {
 	const socket = sock;
 	socket.on('disconnect', () => {
+        console.log("disconnect");
 		socket.leave(room);
+        rooms[room].players--;
 	});
 };
 
@@ -47,6 +72,6 @@ io.sockets.on('connection', (socket) => {
 	console.log('started');
 	
 	onJoined(socket);
-	onDrw(socket);
+	onTurn(socket);
 	onDisconnect(socket);
 });
